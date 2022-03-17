Bug fixed in the normalization scripts
New temporal network(confeat and unet3d added

models/convlstm_network.py
@@ -115,30 +115,30 @@ class SEnDec_cnn_lstm(nn.Module):
 		super(SEnDec_cnn_lstm, self).__init__()
 
 		self.patch_frame_size = patch_frame_size
-		self.seq0 = Conv_block_3d(inp_ch, ch_out=16, batch_norm=False, activation=nn.ReLU(),
+		self.seq0 = Conv_block_3d(inp_ch, ch_out=16, batch_norm=True, activation=nn.ReLU(),
 							 kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1))
 
 		# - SEnDec block 1
 		self.seq1 = Sendec_block(16, ch_out=16)
 
-		self.seq13 = Conv_block_3d(16, ch_out=32, batch_norm=False, activation=nn.ReLU(),
+		self.seq13 = Conv_block_3d(16, ch_out=32, batch_norm=True, activation=nn.ReLU(),
 							 kernel_size=(3, 3, 3), stride=(1, 2, 2), padding=(1, 1, 1))
 
 		# - SEnDec block 2
 		self.seq2 = Sendec_block(32, ch_out=16)
 
-		self.seq22_conv = Conv_block_3d(16, ch_out=32, batch_norm=False, activation=nn.ReLU(),
+		self.seq22_conv = Conv_block_3d(16, ch_out=32, batch_norm=True, activation=nn.ReLU(),
 							 kernel_size=(3, 3, 3), stride=(1, 2, 2), padding=(1, 1, 1))
 
 		# - SEnDec block 3
 		self.seq3 = Sendec_block(32, ch_out=16)
 
-		self.seq3_conv = Conv_block_3d(16, ch_out=32, batch_norm=False, activation=nn.ReLU(),
+		self.seq3_conv = Conv_block_3d(16, ch_out=32, batch_norm=True, activation=nn.ReLU(),
 							 kernel_size=(3, 3, 3), stride=(1, 2, 2), padding=(1, 1, 1))
 
 		self.seq4 = ConvLSTMBlock(32, 16, kernel_size=3, padding=1)
 
-		self.seq5 = Conv_block_3d(16, ch_out=16, batch_norm=False, activation=nn.ReLU(),
+		self.seq5 = Conv_block_3d(16, ch_out=16, batch_norm=True, activation=nn.ReLU(),
 							 kernel_size=(3, 3, 3), stride=(1, 2, 2), padding=(1, 1, 1))
 
 		#-~~~~~~~~~~~~~~~~~~ Upsampling ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -168,10 +168,10 @@ class SEnDec_cnn_lstm(nn.Module):
 							 kernel_size=(3, 3, 3), stride=(1, 1, 1), padding=(1, 1, 1))
 
 		self.seq10 = ConvLSTMBlock(32, 16, kernel_size=3, padding=1)
-		self.seq11 = Conv_block_3d(16, ch_out=16, batch_norm=False, activation=nn.ReLU(),
+		self.seq11 = Conv_block_3d(16, ch_out=16, batch_norm=True, activation=nn.ReLU(),
 							 kernel_size=(3, 3, 3), stride=(2, 1, 1), padding=(0, 1, 1))
 
-		self.seq12 = Conv_block_3d(16, ch_out=16, batch_norm=False, activation=nn.ReLU(),
+		self.seq12 = Conv_block_3d(16, ch_out=16, batch_norm=True, activation=nn.ReLU(),
 								   kernel_size=(3, 3, 3), stride=(2, 1, 1), padding=(0, 1, 1))
 
 		self.out = Conv_block_3d(16, ch_out=1, batch_norm=True, activation=nn.Sigmoid(),

models/temporal_networks.py
@@ -48,7 +48,7 @@ class AvFeat(nn.Module):
 
     def __init__(self, filter_size):
         super().__init__()
-        self.conv1_5x5 = conv_block_3d(1, filter_size, kernel_size=(3, 3, 3), stride=(5, 1, 1), padding=(0, 1, 1))
+        self.conv1_5x5 = conv_block_3d(1, filter_size, kernel_size=(3, 5, 5), stride=(5, 1, 1), padding=(0, 2, 2))
         self.conv1_3x3 = conv_block_3d(1, filter_size, kernel_size=(3, 3, 3), stride=(5, 1, 1), padding=(0, 1, 1))
         self.conv1_1x1 = conv_block_3d(1, filter_size, kernel_size=(3, 1, 1), stride=(5, 1, 1), padding=0)
 
@@ -71,22 +71,31 @@ class AvFeat(nn.Module):
         x1_2 = self.conv1_3x3(inp)
         x1_3 = self.conv1_1x1(inp)
 
+        """
         fused1 = torch.cat((x1_1, x1_2, x1_3), dim=1)
         fused1 = self.conv1_avg_1x1(fused1)
+        """
+        fused1 = (x1_1 + x1_2 + x1_3) / 3
 
         x2_1 = self.conv2_5x5(fused1)
         x2_2 = self.conv2_3x3(fused1)
         x2_3 = self.conv2_1x1(fused1)
 
+        """
         fused2 = torch.cat((x2_1, x2_2, x2_3), dim=1)
         fused2 = self.conv2_avg_1x1(fused2)
+        """
+        fused2 = (x2_1 + x2_2 + x2_3) / 3
 
         x3_1 = self.conv3_5x5(fused2)
         x3_2 = self.conv3_3x3(fused2)
         x3_3 = self.conv3_1x1(fused2)
 
+        """
         fused3 = torch.cat((x3_1, x3_2, x3_3), dim=1)
         fused3 = self.conv3_avg_1x1(fused3)
+        """
+        fused3 = (x3_1 + x3_2 + x3_3) / 3
 
         # 5D to 4D tensor
         with torch.no_grad():
@@ -94,6 +103,41 @@ class AvFeat(nn.Module):
 
         return out
 
+
+class ConFeat(nn.Module):
+
+    @staticmethod
+    def weight_init(m):
+        if isinstance(m, nn.Conv2d):
+            nn.init.kaiming_normal_(m.weight.data, nonlinearity='relu')
+            nn.init.constant_(m.bias.data, 0)
+        elif isinstance(m, nn.Conv3d):
+            nn.init.kaiming_normal_(m.weight.data, nonlinearity='relu')
+            nn.init.constant_(m.bias.data, 0)
+        elif isinstance(m, nn.Linear):
+            nn.init.xavier_normal_(m.weight.data, gain=nn.init.calculate_gain('relu'))
+            nn.init.constant_(m.bias.data, 0)
+
+    def __init__(self, filter_size):
+        super().__init__()
+
+        self.conv1_5x5 = conv_block_3d(1, filter_size, kernel_size=(1, 5, 5), stride=(1, 1, 1), padding=(0, 2, 2))
+        self.conv1_3x3 = conv_block_3d(1, filter_size, kernel_size=(1, 3, 3), stride=(1, 1, 1), padding=(0, 1, 1))
+        self.conv1_1x1 = conv_block_3d(1, filter_size, kernel_size=(1, 1, 1), stride=(1, 1, 1), padding=0)
+
+    def forward(self, inp):
+
+        x1_1 = self.conv1_5x5(inp)
+        x1_2 = self.conv1_3x3(inp)
+        x1_3 = self.conv1_1x1(inp)
+
+        x = (x1_1 + x1_2 + x1_3) / 3
+
+        # 5D to 4D tensor
+        with torch.no_grad():
+            out = x.squeeze(dim=2)
+
+        return out
 
 class TDR(nn.Module):
 

models/unet.py
@@ -4,7 +4,7 @@
 import torch
 import torch.nn as nn
 from models.unet_tools import UNetDown, UNetUp, ConvSig
-from models.temporal_networks import AvFeat, TDR
+from models.temporal_networks import AvFeat, TDR, ConFeat
 
 class unet_vgg16(nn.Module):
     """
@@ -31,6 +31,10 @@ class unet_vgg16(nn.Module):
 
         if 'avfeat' in self.temporal_network:
             self.AvFeat = AvFeat(filter_size=filter_size)
+            inp_ch = inp_ch + filter_size
+
+        if 'confeat' in self.temporal_network:
+            self.ConFeat = ConFeat(filter_size=filter_size)
             inp_ch = inp_ch + filter_size
 
         if 'tdr' in self.temporal_network:
@@ -77,6 +81,11 @@ class unet_vgg16(nn.Module):
                 temporal_network_res = self.AvFeat(temporal_patch)
             elif self.temporal_network == 'tdr':
                 temporal_network_res = self.TDR(temporal_patch.squeeze(dim=1))
+            elif self.temporal_network == "avfeat_confeat":
+                avfeat = self.AvFeat(temporal_patch)
+                confeat = self.ConFeat(temporal_patch[:, :, -1, :, :].unsqueeze(dim=1)) # give current frame to network
+                temporal_network_res = torch.cat((avfeat, confeat), dim=1)
+
             elif self.temporal_network == 'avfeat_tdr':
                 avfeat = self.AvFeat(temporal_patch)
                 tdr = self.TDR(temporal_patch.squeeze(dim=1))

utils/augmentations.py
@@ -455,17 +455,17 @@ class NormalizeTensor:
 
         c, h, w = inp.shape
 
-        normalized_frame = int(1*self.empty_bg + 1*self.recent_bg + 1*self.current_fr + 1*self.patch_frame_size)
-        normalized_channel_size = normalized_frame * (1 + 1*self.segmentation_ch)
+        normalized_curr_frame = int(1*self.empty_bg + 1*self.recent_bg + 1*self.current_fr)
 
-        mean_vec = np.concatenate([mean_period for _ in range(normalized_frame)])
-        std_vec = np.concatenate([std_period for _ in range(normalized_frame)])
+        mean_vec = np.concatenate([mean_period for _ in range(normalized_curr_frame)]
+                                 + [self.mean_rgb for _ in range(c - normalized_curr_frame)])
+        std_vec = np.concatenate([std_period for _ in range(normalized_curr_frame)]
+                                 + [self.std_rgb for _ in range(c - normalized_curr_frame)])
 
-        inp_n = tvtf.Normalize(mean_vec, std_vec)(inp[:normalized_channel_size])
-        if c > normalized_channel_size:
-            inp_n = torch.cat((inp_n, inp[normalized_channel_size:]), dim=0)
+        inp = tvtf.Normalize(mean_vec, std_vec)(inp)
 
-        return inp_n, out
+        return inp, out
+
 
 def _centerCrop(im, w_, h_):
     """